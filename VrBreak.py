1333fa14136c722ec5253abaebc4b58cb7650ff2
EvanMPutnam <emput1998@gmail.com>
2017-07-20 07:21:38 -0400

    Create VrBreak.py
    Implements a key press of the f1 key to interrupt the htc vive when pressing an amazon dash button.
    
    You need to hard code the mac address values of your device.

---
+++ VrBreak.py
@@ -0,0 +1,28 @@
+"""
+Author: Evan Putnam
+Description: Implements a key press of the f1 key to interrupt the htc vive when pressing an amazon dash button.
+Pre-Reqs: scapy-python3, pyautogui
+
+#pip install scapy-python3
+#pip install pyautogui
+"""
+from scapy.all import *
+import pyautogui
+
+def knockKnock():
+    pyautogui.press('f1')
+
+def arpCallback(pkt):
+    if ARP in pkt and pkt[ARP].op == 1:
+        #Need to hard code the mac address below
+        if pkt[ARP].hwsrc == '':
+            print("starting")
+            knockKnock()
+            return pkt.sprintf("Button Pressed!")
+
+
+def main():
+    sniff(prn=arpCallback, filter="arp", store=0, count=0)
+
+if __name__ == '__main__':
+    main()
